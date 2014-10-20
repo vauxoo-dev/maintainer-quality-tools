d39e9a1d91064cac8123a207e34d6aaa11390f89
moylop260 <moylop260@vauxoo.com>
2014-10-20 00:54:22 -0500

    [FIX][test_module] Fix flake8 E128 continuation line

--- tests/test_repo/test_module/dummy_model.py
+++ tests/test_repo/test_module/dummy_model.py
@@ -15,4 +15,4 @@ class DummyModel(orm.Model):
 # printout non-ASCII text to check unicode issues
 # (from Monty Python's "Italian Lesson"...)
 sys.stdout.write("Eeeeeee! Milano è tanto meglio di Napoli. "
-      "Milano è la citta la più bella di tutti ... nel mondo...\n")
+                 "Milano è la citta la più bella di tutti ... nel mondo...\n")
