9ba198c4c8653600a9a1269b2bd333d190d13c3b
Moises Lopez <moylop260@vauxoo.com>
2015-08-14 02:40:45 +0000

    SQUASH

--- tests/test_repo/broken_module/model.py
+++ tests/test_repo/broken_module/model.py
@@ -6,3 +6,16 @@ class test_model(orm.Model):
     _columns = {
         'name': fields.char('Title', 100),
     }
+
+    def __init__(self):
+        return None
+
+    def method_test(self, param1, param2):
+        pass
+
+    def method_e1124(self):
+        self.method1('value_param1', param1='value_param1')
+
+    def method_e1306(self):
+        return "%s %s"%('value1')
+
