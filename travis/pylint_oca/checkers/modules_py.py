7f4768c8758df74c5d2ed01341a6d90f82198d73
Moises Lopez <moylop260@vauxoo.com>
2015-08-22 21:45:54 -0500

    squash

--- travis/pylint_oca/checkers/modules_py.py
+++ travis/pylint_oca/checkers/modules_py.py
@@ -10,7 +10,7 @@ OCA_MSGS = {
     # Visit py module
     'C%d01' % settings.BASE_PYMODULE_ID: (
         'No UTF-8 coding found: Use `# -*- coding: utf-8 -*-` '
-        'in first or second line of your py file.',
+        'in first or second line.',
         'no-utf8-coding-comment',
         'ToDo: Msg tmpl...'
     ),
