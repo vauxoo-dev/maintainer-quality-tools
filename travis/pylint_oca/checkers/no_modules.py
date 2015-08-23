f3c41b203408ea427eef9fefdb397883a2418f44
Moises Lopez <moylop260@vauxoo.com>
2015-08-22 22:33:18 -0500

    squash

---
+++ travis/pylint_oca/checkers/no_modules.py
@@ -0,0 +1,65 @@
+
+from pylint.checkers import BaseChecker
+from pylint.interfaces import IAstroidChecker
+
+from .. import settings
+
+
+OCA_MSGS = {
+    # C->convention R->refactor W->warning E->error F->fatal
+
+    'R%d01' % settings.BASE_NOMODULE_ID: (
+        'Import `Warning` should be renamed as UserError '
+        '`from openerp.exceptions import Warning as UserError`',
+        'openerp-exception-warning',
+        MSG_TMPL
+    ),
+    'W%d01' % settings.BASE_NOMODULE_ID: (
+        'Detected api.one and api.multi decorators together.',
+        'api-one-multi-together',
+        MSG_TMPL
+    ),
+    'W%d04' % settings.BASE_NOMODULE_ID: (
+        'Missing api.one in copy function.',
+        'copy-wo-api-one',
+        MSG_TMPL
+    ),
+}
+
+
+class WrapperModuleChecker(BaseChecker):
+
+    __implements__ = IAstroidChecker
+
+    name = settings.CFG_SECTION
+    msgs = OCA_MSGS
+
+
+    @utils.check_messages('api-one-multi-together',
+                          'copy-wo-api-one')
+    def visit_function(self, node):
+        '''Check that `api.one` and `api.multi` decorators not exists together
+        Check that method `copy` exists `api.one` decorator
+        '''
+        decor_names = self.get_decorators_names(node.decorators)
+        decor_lastnames = [
+            decor.split('.')[-1]
+            for decor in decor_names]
+        if self.linter.is_message_enabled('api-one-multi-together'):
+            if 'one' in decor_lastnames \
+                    and 'multi' in decor_lastnames:
+                self.add_message('api-one-multi-together',
+                                 node=node)
+
+        if self.linter.is_message_enabled('copy-wo-api-one'):
+            if 'copy' == node.name and 'one' not in decor_lastnames:
+                self.add_message('copy-wo-api-one', node=node)
+
+    @utils.check_messages('openerp-exception-warning')
+    def visit_from(self, node):
+        if node.modname == 'openerp.exceptions':
+            for (import_name, import_as_name) in node.names:
+                if import_name == 'Warning' \
+                        and import_as_name != 'UserError':
+                    self.add_message(
+                        'openerp-exception-warning', node=node)
