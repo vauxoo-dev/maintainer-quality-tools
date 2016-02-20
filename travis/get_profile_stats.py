3cf83b6272f8f56a6f5c5374363b2989c43c2a7a
Moisés López <moylop260@vauxoo.com>
2016-02-20 02:14:00 +0000

    [IMP] fnames exclude

--- travis/get_profile_stats.py
+++ travis/get_profile_stats.py
@@ -6,9 +6,11 @@ import pstats
 import sys
 
 
-def print_stats(filter_fnames=None):
+def print_stats(filter_fnames=None, exclude_fnames=None):
     if filter_fnames is None:
         filter_fnames = ['.py']
+    if exclude_fnames is None:
+        exclude_fnames = []
     fname = os.path.expanduser('~/.openerp_server.stats')
     fstats = pstats.Stats(fname)
     stats = fstats.stats
@@ -18,6 +20,13 @@ def print_stats(filter_fnames=None):
         for tuple_stats in stats[stat][4].keys():
             for filter_fname in filter_fnames:
                 if filter_fname in tuple_stats[0]:
+                    exclude = False
+                    for exclude_fname in exclude_fnames:
+                        if exclude_fname in tuple_stats[0]:
+                            exclude = True
+                            break
+                    if exclude:
+                        break
                     old_fstats = stats_filter.setdefault(
                         tuple_stats, (0, 0, 0, 0))
                     new_fstats = stats[stat][4][tuple_stats]
@@ -25,6 +34,7 @@ def print_stats(filter_fnames=None):
                         old_item + new_item
                         for old_item, new_item in zip(old_fstats, new_fstats)])
                     stats_filter[tuple_stats] = sum_fstats
+                    break
 
     def sort_stats(dict_stats, index=0, reverse=True):
         """param index: Index of tuple stats standard to sort
@@ -43,4 +53,6 @@ def print_stats(filter_fnames=None):
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.argv.append('.py')
-    print_stats(sys.argv[1:])
+    fnames_filter = sys.argv[1].split(',')
+    fnames_exclude = sys.argv[2].split(',')
+    print_stats(fnames_filter, fnames_exclude)
