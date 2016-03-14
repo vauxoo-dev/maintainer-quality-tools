8fccecb6a08a770397eb151e1e37c35bcd87c3ef
Moisés López <moylop260@vauxoo.com>
2016-03-14 06:12:47 +0000

    squash

--- travis/psql_log.py
+++ travis/psql_log.py
@@ -11,6 +11,43 @@ def get_psql_conf_files(psql_conf_path=None):
         yield fname_conf
 
 
+def get_default_params_log_server(extra_params=None):
+    if extra_params is None:
+        extra_params = []
+    params = [
+        "logging_collector=on",
+        "log_destination='stderr'",
+        "log_directory='pg_log'",
+        "log_filename='postgresql.log'",
+        "log_rotation_age=0",
+        "log_checkpoints=on",
+        "log_hostname=on",
+        "log_line_prefix='%t [%p]: [%l-1] db=%d,user=%u '",
+    ]
+    params.extend(extra_params)
+    return params
+
+
+def get_default_params_log_client(extra_params=None):
+    if extra_params is None:
+        extra_params = []
+    params = [
+        "client_min_messages=notice",
+        "log_min_messages=warning",
+        "log_min_error_statement=error",
+        "log_min_duration_statement=0",
+        "log_connections=on",
+        "log_disconnections=on",
+        "log_duration=off",
+        "log_error_verbosity=verbose",
+        "log_lock_waits=on",
+        "log_statement=none",
+        "log_temp_files=0",
+    ]
+    params.extend(extra_params)
+    return params
+
+
 def enable_psql_logs(psql_conf_path=None):
     """Enable psql logs.
     This change require receive from psql follow environment variables
@@ -18,18 +55,12 @@ def enable_psql_logs(psql_conf_path=None):
     """
     for fname_conf in get_psql_conf_files():
         with open(fname_conf, "a+") as fconf:
-            if 'logging_collector = on\n' in fconf.readlines():
+            param_list = get_default_params_log_server()
+            if param_list[0] + '\n' in fconf.readlines():
                 continue
             print("Enable logs to", fname_conf)
-            fconf.write("""
-logging_collector = on
-log_destination = 'stderr'
-log_directory = 'pg_log'
-log_filename = 'postgresql.log'
-log_rotation_age = 0
-log_checkpoints = on
-log_hostname = on
-log_line_prefix = '%t [%p]: [%l-1] db=%d,user=%u '""")
+            param_str = '\n'.join(param_list)
+            fconf.write(param_str)
     subprocess.call("/etc/init.d/postgresql restart", shell=True)
 
 
@@ -39,7 +70,10 @@ def get_env_log(env=None):
         env = {}
     custom_env = env.copy()
     custom_env.setdefault('PGOPTIONS', '')
-    custom_env['PGOPTIONS'] += " -c client_min_messages=notice -c log_min_messages=warning -c log_min_error_statement=error -c log_min_duration_statement=0 -c log_connections=on -c log_disconnections=on -c log_duration=off -c log_error_verbosity=verbose -c log_lock_waits=on -c log_statement=none -c log_temp_files=0"  # noqa
+    param_list = get_default_params_log_client()
+    sep = ' -c '
+    param_str = sep + sep.join(param_list)
+    custom_env['PGOPTIONS'] += param_str
     custom_env['PGOPTIONS'] = custom_env['PGOPTIONS'].strip(' ')
     return custom_env
 
