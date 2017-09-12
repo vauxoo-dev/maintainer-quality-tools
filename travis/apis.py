8fc4d2ca5cfaf7337ed33edeb80234125c2d458d
JesusZapata <jesus@vauxoo.com>
2017-09-12 20:13:21 +0000

    [FIX] apis.py: Adding new metod _ssh_keyscan to run the ssh-keyscan over the weblate-ssh server

--- travis/apis.py
+++ travis/apis.py
@@ -2,6 +2,8 @@
 
 import base64
 import os
+import re
+import subprocess
 import tempfile
 import time
 import json
@@ -54,6 +56,18 @@ class WeblateApi(Request):
         self.ssh = os.environ.get(
             "WEBLATE_SSH", "ssh://user@webpage.com")
         self.tempdir = os.path.join(tempfile.gettempdir(), 'weblate_api')
+        self._ssh_keyscan()
+
+    def _ssh_keyscan(self):
+        cmd = "ssh-keyscan -p %s %s"
+        match = re.search(r'@(?P<port_host>[^/]+)', self.ssh)
+        if match:
+            port_host = match.groupdict()['port_host'].split(':')
+            host, port = ((port_host[0], 22) if len(port_host) == 1 else
+                          (port_host[0], port_host[1]))
+            cmd = cmd % (port, host)
+            with open(os.path.expanduser('~/.ssh/known_hosts'), 'w') as hosts:
+                subprocess.Popen(cmd.split(), stdout=hosts)
 
     def get_project(self, repo_slug, branch):
         self.branch = branch
